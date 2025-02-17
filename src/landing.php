367f4b518c8ed2e6a8d24a2bee8d82a6f701a19c
Brian Diep <bdiep6@my.bcit.ca>
2025-02-16 19:08:57 -0800

    Added fonts to landing

--- src/landing.php
+++ src/landing.php
@@ -3,6 +3,11 @@
 <?php include("./inc_header.php"); ?>
 <?php include("./inc_db_params.php"); ?> 
 <?php include("./seed.php") ?>
+<!-- font -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text:ital@0;1&display=swap" rel="stylesheet">
+
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 <link href="landing-css.css" rel="stylesheet">
 
