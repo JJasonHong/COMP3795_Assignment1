4b694d781677aa0f168654738ecbb8b91cdc7fda
JJasonHong <jasonr8hong@hotmail.ca>
2025-02-18 15:42:19 -0800

    fixed the delete.php bug

--- src/inc_footer.php
+++ src/inc_footer.php
@@ -5,8 +5,9 @@
             <div class="col-md-12 text-center">
                 <p>Created by:
                     <strong>Team Members:</strong>
-                    John Doe (A01234567),
-                    Jane Smith (A01234568)
+                    Gem Sha A01345766,
+                    Brian Diep A00959233,
+                    Jason Hong A01232139
                 </p>
             </div>
         </div>
