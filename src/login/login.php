ce8003ba72c605c0f77e9802eb67615bab85ec7a
Brian Diep <bdiep6@my.bcit.ca>
2025-02-18 16:14:51 -0800

    added bg color

--- src/login/login.php
+++ src/login/login.php
@@ -54,3 +54,17 @@ if (isset($_SESSION['username'])) {
 <!-- Login Page END -->
 
 <?php include("../inc_footer.php"); ?>
+<!-- Styling -->
+<?php
+echo "<style>
+        body {
+            background-color: #d9b99b;
+        }
+
+        form {
+            background-color: #faf0e6;
+            padding: 10px;
+            border-radius: 10px;
+        }
+      </style>";
+?>
